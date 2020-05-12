f752f5bd5b9671147b62e65df4e9c86fd0872f93
amine <amine.amirouche96@hotmail.com>
2020-05-12 02:19:00 +0200

    j'ajoute la premiere question

---
+++ src/main/java/uvsq/pglp_4_1/Personnel.java
@@ -0,0 +1,55 @@
+package uvsq.pglp_4_1;
+
+import java.util.ArrayList;
+
+public final class Personnel {
+	
+	private  String Nom;
+	private  String Prenom;
+	private  String Post;
+	private  java.time.LocalDate Date;
+	private  ArrayList<Integer> Tel;
+	
+	Personnel(Builder build)
+	{
+		Nom=build.Nom;
+		Prenom=build.Prenom;
+		Post=build.Post;
+		Date=build.Date;
+		Tel=build.Tel;
+	}
+	public static class Builder {
+		
+		private final String Nom;
+		private final String Prenom;
+		private final java.time.LocalDate Date;
+		
+		private  String Post="inconnue";		
+		private  ArrayList<Integer> Tel=null;
+		
+		
+		public Builder(String Nom,String Prenom,java.time.LocalDate Date)
+		{
+			this.Date=Date;
+			this.Nom=Nom;
+			this.Prenom=Prenom;
+		}
+		
+		public Builder AddTel(int numero)
+		{
+			this.Tel.add(numero);
+			return this;
+		}
+		
+		public Personnel build()
+		{
+			return new Personnel(this);
+		}
+	}
+	public void print() {
+		System.out.println("Nom du personnel" + Nom );
+		
+	}
+	
+
+}
